6d8313b2f5fd33f9b0a3d716da3aeaebb1aeadbe
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2023-10-18 18:24:05 -0500

    Time: 47 ms (66.36%), Space: 60.8 MB (69.12%) - LeetHub

---
+++ 2096-find-the-longest-valid-obstacle-course-at-each-position/2096-find-the-longest-valid-obstacle-course-at-each-position.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
+        int n = obstacles.length;
+        int[] stack = new int[n];
+        int[] ans = new int[n];
+        int length = 0;
+
+        for(int i = 0; i < n; i++) {
+            int start = 0, end = length;
+            while(start < end) {
+                int mid = start + (end - start)/2;
+                if(stack[mid] <= obstacles[i])
+                    start = mid + 1;
+                else
+                    end = mid;
+            }
+            if(start == length) {
+                length++;
+            }
+            stack[start] = obstacles[i];
+            ans[i] = start + 1;
+            // System.out.println(Arrays.toString(stack) + " " + start);
+        }
+        // System.out.println("-------------------");
+        return ans;
+    }
+}
