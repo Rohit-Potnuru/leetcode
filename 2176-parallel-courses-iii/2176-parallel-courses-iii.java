57f34b121010e8282011fc3c811c4deb9be7ca12
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2023-10-17 23:06:17 -0500

    Time: 17 ms (94.67%), Space: 67.2 MB (65.33%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.java
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.java
@@ -1,19 +1,13 @@
 class Solution {
     public int minimumTime(int n, int[][] relations, int[] time) {
-        int[] timeTaken = new int[n];
-        for(int i = 0; i < n; i++) {
-            timeTaken[i] = time[i];
-        }
 
         //for Creating adjacent matrix
         int src, dest;
+        int[] timeTaken = new int[n];
         ArrayList<Integer>[] adjMatrix = new ArrayList[n];
         int[] inDegree = new int[n];
         //initialization
-        for(int i = 0; i < n; i++) {
-            adjMatrix[i] = new ArrayList<>();
-            inDegree[i] = 0;
-        }
+        for(int i = 0; i < n; i++) adjMatrix[i] = new ArrayList<>();
 
         //generating adj matrix
         for(int[] relation: relations) {
@@ -23,17 +17,20 @@ class Solution {
             adjMatrix[src].add(dest);
         }
 
-        int count = n;
-        while(count > 0) {
-            count = 0;
-            for(int i = 0; i < n; i++) {
-                if(inDegree[i] == 0) {
-                    inDegree[i]--;
-                    for(Integer d: adjMatrix[i]) {
-                        timeTaken[d] = Math.max(timeTaken[d], timeTaken[i] + time[d]);
-                        inDegree[d]--;
-                    }
-                    count++;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
+            if(inDegree[i] == 0) {
+                timeTaken[i] = time[i];
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()) {
+            src = q.poll();
+            for(Integer d: adjMatrix[src]) {
+                timeTaken[d] = Math.max(timeTaken[d], timeTaken[src] + time[d]);
+                if(--inDegree[d] == 0) {
+                    q.add(d);
                 }
             }
         }
@@ -45,14 +42,3 @@ class Solution {
         return maxTime;
     }
 }
-/*
-1
-2    5
-3 4
-
-1 0
-2 0
-3 0
-4 1
-5 4
-*/
