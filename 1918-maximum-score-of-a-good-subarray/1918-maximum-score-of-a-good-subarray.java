6981707fc00ab6fad2fb6210014691d5a9ea5a14
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2023-10-22 20:46:24 -0500

    Time: 8 ms (72.38%), Space: 59.2 MB (31.43%) - LeetHub

--- 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.java
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.java
@@ -4,30 +4,17 @@ class Solution {
         int left = k, right = k;
         int currMin = nums[k];
         int maxScore = nums[k];
-        while(left >= 0 || right < n) {
-            while(left >= 0 && nums[left] >= currMin)
-                left--;
-            
-            while(right < n && nums[right] >= currMin)
+        while(left > 0 || right < n - 1) {
+            if(left == 0)
                 right++;
-
-            maxScore = Math.max(maxScore, currMin * (right - left - 1));
-            if(left < 0 && right >= n) 
-                continue;
-            if(left < 0)
-                currMin = nums[right];
-            else if(right >= n)
-                currMin = nums[left];
-            else {
-                if(nums[left] < nums[right]) {
-                    currMin = nums[right];
-                    left++;
-                }
-                else {
-                    currMin = nums[left];
-                    right--;
-                }
-            }
+            else if(right == n - 1)
+                left--;
+            else if(nums[left - 1] < nums[right + 1]) 
+                right++;
+            else
+                left--;
+            currMin = Math.min(currMin, Math.min(nums[left], nums[right]));
+            maxScore = Math.max(maxScore, currMin * (right - left + 1));
         }
         return maxScore;
     }
