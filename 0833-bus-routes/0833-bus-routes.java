f855710a5726e9de2df711da58c8220e736728b3
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2023-10-24 12:18:56 -0500

    Time: 748 ms (25.54%), Space: 73.2 MB (54.57%) - LeetHub

--- 0833-bus-routes/0833-bus-routes.java
+++ 0833-bus-routes/0833-bus-routes.java
@@ -1,9 +1,10 @@
 class Solution {
     public int numBusesToDestination(int[][] routes, int source, int target) {
+        int n = routes.length;
         if (source == target) return 0;
 
         Map<Integer, List<Integer>> stopToRoutes = new HashMap<>();
-        for (int i = 0; i < routes.length; i++) {
+        for (int i = 0; i < n; i++) {
             for (int stop : routes[i]) {
                 stopToRoutes.putIfAbsent(stop, new ArrayList<>());
                 stopToRoutes.get(stop).add(i);
@@ -11,11 +12,13 @@ class Solution {
         }
 
         Set<Integer> visitedRoutes = new HashSet<>();
+        boolean[] seen_routes = new boolean[n];
         Queue<Integer> queue = new LinkedList<>();
         int level = 0;
 
         // Initialize the queue with routes that have the source stop.
         for (int route : stopToRoutes.get(source)) {
+            seen_routes[route] = true;
             queue.offer(route);
             visitedRoutes.add(route);
         }
@@ -29,7 +32,7 @@ class Solution {
                 for (int nextStop : routes[currentRoute]) {
                     if (nextStop == target) return level;  // Found the target.
                     for (int nextRoute : stopToRoutes.get(nextStop)) {
-                        if (!visitedRoutes.contains(nextRoute)) {
+                        if (!visitedRoutes.contains(nextRoute) && !seen_routes[nextRoute]) {
                             visitedRoutes.add(nextRoute);
                             queue.offer(nextRoute);
                         }
