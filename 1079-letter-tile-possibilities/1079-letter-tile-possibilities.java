c81eb3226914c9b1c1dddf1a0f3cfe853708c9fd
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2023-12-15 14:45:26 -0600

    Time: 2 ms (88.43%), Space: 40.9 MB (74.89%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalPermutation(int[] freq) {
+        int total = 1;
+        for(int i = 0; i < freq.length; i++) {
+            if(freq[i] != 0) {
+                freq[i] -= 1;
+                total += totalPermutation(freq);
+                freq[i] += 1;
+            } 
+        }
+        return total;
+    }
+    public int numTilePossibilities(String tiles) {
+        int[] freq = new int[26];
+        for(int i = 0; i < tiles.length(); i++) {
+            freq[tiles.charAt(i) - 'A'] += 1;
+        }
+
+        return totalPermutation(freq) - 1;
+    }
+}
