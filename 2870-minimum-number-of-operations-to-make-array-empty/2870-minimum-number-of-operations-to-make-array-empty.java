d829e52a5d482d7aaa6e9971249c13db9bf3d91d
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-01-04 00:00:06 -0600

    Time: 5 ms (100%), Space: 57.7 MB (36.98%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,20 +1,20 @@
 class Solution {
     public int minOperations(int[] nums) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for(int num: nums) {
-            if(!freq.containsKey(num)) {
-                freq.put(num, 0);
-            }
-            freq.put(num, freq.get(num) + 1);
-        }
-
+        Arrays.sort(nums);
         int count = 0;
-        for(int num: freq.keySet()) {
-            if(freq.get(num) == 1) {
-                return -1;
+        int prev = nums[0] - 1;
+        int prevCount = 0;
+        for(int num: nums) {
+            if(prev != num) {
+                if(prevCount == 1) return -1;
+                count += prevCount/3 + (prevCount%3 + 1)/2;
+                prevCount = 0;
+                prev = num;
             }
-            count += freq.get(num)/3 + (freq.get(num) % 3 + 1)/2;
+            prevCount++;
         }
+        if(prevCount == 1) return -1;
+        count += prevCount/3 + (prevCount%3 + 1)/2;
         return count;
     }
 }
