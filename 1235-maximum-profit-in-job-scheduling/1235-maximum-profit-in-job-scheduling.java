884aaa8e63776cfe7e93b10c8ae524993736844d
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-01-06 12:42:26 -0600

    Time: 25 ms (92.07%), Space: 55.1 MB (70.93%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,19 +1,17 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int n = profit.length;
-        int[][] nums = new int[n][3];
+        Integer[] nums = new Integer[n];
         for(int i = 0; i < n; i++) {
-            nums[i][0] = startTime[i];
-            nums[i][1] = endTime[i];
-            nums[i][2] = profit[i];
+            nums[i] = i;
         }
 
-        Arrays.sort(nums, (a, b) -> a[1] - b[1]);
+        Arrays.sort(nums, Comparator.comparingInt(i -> endTime[i]));
         for(int i = 1; i < n; i++) {
             int mid, start = 0, end = i;
             while(start < end) {
                 mid = (end - start)/2 + start;
-                if(nums[mid][1] <= nums[i][0]) {
+                if(endTime[nums[mid]] <= startTime[nums[i]]) {
                     start = mid + 1;
                 }
                 else {
@@ -21,10 +19,10 @@ class Solution {
                 }
             }
             if(start != 0) {
-                nums[i][2] += nums[start - 1][2];
+                profit[nums[i]] += profit[nums[start - 1]];
             }
-            nums[i][2] = Math.max(nums[i - 1][2], nums[i][2]);
+            profit[nums[i]] = Math.max(profit[nums[i - 1]], profit[nums[i]]);
         }
-        return nums[n - 1][2];
+        return profit[nums[n - 1]];
     }
 }
