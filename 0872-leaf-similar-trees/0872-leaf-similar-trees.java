27688a4243705ba775c13656fdc912e7877c3558
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-01-08 19:25:08 -0600

    Time: 0 ms (100%), Space: 41.7 MB (6.61%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -28,18 +28,13 @@ class Solution {
     }
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> curr1 = new ArrayList<Integer>();
+        List<Integer> curr2 = new ArrayList<Integer>();
+
         temp = curr1;
         leafNode(root1);
-        List<Integer> curr2 = new ArrayList<Integer>();
+        
         temp = curr2;
         leafNode(root2);
-        if(curr1.size() != curr2.size()) {
-            return false;
-        }
-        for(int i = 0; i < curr1.size(); i++) {
-            if(curr1.get(i) != curr2.get(i))
-                return false;
-        }
-        return true;
+        return curr1.equals(curr2);
     }
 }
