2b667079481ff05ef3b7f526ea3376d623e3f4d5
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-01-10 19:50:35 -0600

    Time: 0 ms (100%), Space: 41.8 MB (32.81%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -14,26 +14,32 @@
  * }
  */
 class Solution {
-    int maxValue = 0;
-    private Pair<Integer, Integer> postOrder(TreeNode root) {
-        if(root == null) {
-            return new Pair<>(Integer.MAX_VALUE, Integer.MIN_VALUE);
+    int maxDiff;
+    public int[] dfs(TreeNode root) {
+        int[] temp = new int[2], temp1, temp2;
+        temp[0] = Integer.MAX_VALUE;
+        temp[1] = Integer.MIN_VALUE;
+        if(root.left != null) {
+            temp = dfs(root.left);
+            this.maxDiff = Math.max(Math.abs(temp[0] - root.val), this.maxDiff);
+            this.maxDiff = Math.max(Math.abs(temp[1] - root.val), this.maxDiff);
         }
-        else {
-            int max = root.val, min = root.val;
-            Pair temp = postOrder(root.left);
-            min = Math.min(min, (int)temp.getKey());
-            max = Math.max(max, (int)temp.getValue());
-            temp = postOrder(root.right);
-            min = Math.min(min, (int)temp.getKey());
-            max = Math.max(max, (int)temp.getValue());
-            maxValue = Math.max(Math.abs(root.val - max), maxValue);
-            maxValue = Math.max(Math.abs(root.val - min), maxValue);
-            return new Pair<>(min, max);
+        if(root.right != null) {
+            temp1 = dfs(root.right);
+            temp[0] = Math.min(temp[0], temp1[0]);
+            temp[1] = Math.max(temp[1], temp1[1]);
+            this.maxDiff = Math.max(Math.abs(temp[0] - root.val), this.maxDiff);
+            this.maxDiff = Math.max(Math.abs(temp[1] - root.val), this.maxDiff);
         }
+        temp[0] = Math.min(temp[0], root.val);
+        temp[1] = Math.max(temp[1], root.val);
+        return temp;
     }
     public int maxAncestorDiff(TreeNode root) {
-        postOrder(root);
-        return maxValue;
+        this.maxDiff = 0;
+        if(root != null) {
+            dfs(root);
+        }
+        return maxDiff;
     }
 }
