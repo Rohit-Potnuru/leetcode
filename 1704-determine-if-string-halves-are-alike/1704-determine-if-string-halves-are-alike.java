3d060855584e7bcef68fbbdd1638f42ca22838c0
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-01-11 23:31:59 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,12 +3,11 @@ class Solution {
         int n = s.length(), count = 0;
         Set<Character> vowels = new HashSet<>();
         vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
-        vowels.add('A'); vowels.add('E'); vowels.add('I'); vowels.add('O'); vowels.add('U');
         for(int i = 0; i < n/2; i++) {
             if(vowels.contains(s.charAt(i))) {
                 count++;
             }
-            if(vowels.contains(s.charAt(n/2 + i))) {
+            if(vowels.contains(s.charAt(n/2 + i).toLowerCase(ch))) {
                 count--;
             }
         }
