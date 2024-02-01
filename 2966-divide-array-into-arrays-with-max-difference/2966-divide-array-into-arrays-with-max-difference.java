a9c55d31055df201a19b8677d69c05150dc9c997
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-01-31 22:01:41 -0600

    Time: 28 ms (18.66%), Space: 59.6 MB (8.79%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -3,22 +3,19 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
         int[][] res = new int[n/3][3];
-        int prev = -1, max = 0, min = 0;
-        for(int i = 0; i < n; i++) {
-            if(prev != i/3) {
-                if(max - min > k) {
-                    return new int[0][0];
-                }
-                max = nums[i];
-                min = nums[i];
+        int max = 0, min = 0;
+        for(int i = 0; i < n/3; i++) {
+            max = Integer.MIN_VALUE;
+            min = Integer.MAX_VALUE;
+            for(int j = 0; j < 3; j++) {
+                res[i][j] = nums[3 * i + j];
+                max = Math.max(max, res[i][j]);
+                min = Math.min(min, res[i][j]);
             }
-            res[i/3][i%3] = nums[i];
-            max = Math.max(max, nums[i]);
-            min = Math.min(min, nums[i]);
-            prev = i/3;
-        }
-        if(max - min > k) {
-            return new int[0][0];
+            if(max - min > k) {
+                System.out.println(min + " " + max);
+                return new int[0][0];
+            }
         }
         return res;
     }
