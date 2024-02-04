7004fdab06de6e26343b0a341983f39fcf36c723
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-04 16:00:39 -0600

    Time: 2 ms (98.08%), Space: 43.5 MB (95.87%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,31 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n = s.length(), uniqCount = 0;
-        HashMap<Character, Integer> freqt = new HashMap<>();
+        int n = s.length();
+        int[] freqt = new int[128];
+        int len_t = t.length();
         for(char ch: t.toCharArray()) {
-            freqt.put(ch, freqt.getOrDefault(ch, 0) + 1);
-            if(freqt.get(ch) == 1) {
-                uniqCount++;
-            }
-        }
+            freqt[ch]++;
+        }   
 
-        int j = 0, i = 0;
-        int minCount = Integer.MAX_VALUE, startMin = -1;
-        char ch;
-        HashMap<Character, Integer> freqs = new HashMap<>();
-        while(j < n) {
-            ch = s.charAt(j);
-            freqs.put(ch, freqs.getOrDefault(ch, 0) + 1);
-            if(freqs.get(ch).equals(freqt.getOrDefault(ch, 0))) {
-                uniqCount--;
+        int start = 0, end = 0;
+        int minLen = Integer.MAX_VALUE, startIndex = -1;
+        while(end < n) {
+            if(freqt[s.charAt(end++)]-- > 0) {
+                len_t--;
             }
-            j++;
 
-            while(uniqCount == 0) {
-                if(minCount > j - i) {
-                    minCount = j - i;
-                    startMin = i;
+            while(len_t == 0) {
+                if(minLen > end - start) {
+                    startIndex = start;
+                    minLen = end - start;
                 }
 
-                ch = s.charAt(i);
-                freqs.put(ch, freqs.get(ch) - 1);
-                if(freqs.get(ch) < freqt.getOrDefault(ch, 0)) {
-                    uniqCount++;
+                if(freqt[s.charAt(start++)]++ == 0) {
+                    len_t++;
                 }
-                i++;
             }
         }
-        
-        return (startMin >= 0) ? s.substring(startMin, startMin + minCount): "";
+        return (startIndex >= 0) ? s.substring(startIndex, startIndex + minLen): "";
     }
 }
 /*
