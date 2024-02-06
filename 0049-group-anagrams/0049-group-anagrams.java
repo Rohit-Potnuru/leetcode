f707a86b05487e69040eac594ed4a9af49c80156
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-05 18:51:43 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,9 +5,16 @@ class Solution {
         String temp;
 
         for(String s: strs) {
-            char[] s_c = s.toCharArray();
-            Arrays.sort(s_c);
-            temp = new String(s_c);
+            int[] count = new int[26];
+            for(char ch: s.toCharArray()) {
+                count[ch - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(int i: count) {
+                sb.append(i); 
+            }
+            temp = sb.toString();
             if(!map.containsKey(temp)) {
                 map.put(temp, res.size());
                 res.add(new ArrayList<>());
