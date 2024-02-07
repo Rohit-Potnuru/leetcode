755aa9e0087c054dbdc82b912aa78eff233e95ea
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-06 19:20:34 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,23 +1,25 @@
 class Solution {
     public String frequencySort(String s) {
         StringBuilder[] freqSB = new StringBuilder[128];
-        for(int i = 0; i < 128; i++) {
-            freqSB[i] = new StringBuilder();
-        }
         for(char ch: s.toCharArray()) {
+            if(freqSB[ch] == null) {
+                freqSB[ch] = new StringBuilder();
+            }
             freqSB[ch].append(ch);
         }
 
         Arrays.sort(freqSB, new Comparator<StringBuilder>() {
             @Override
             public int compare(StringBuilder s1, StringBuilder s2) {
-                return s2.length() - s1.length();
+                if(s1 == null) return 1;
+                else if(s2 == null) return -1;
+                else return s2.length() - s1.length();
             }
         });
 
         StringBuilder res = new StringBuilder();
         for(int i = 0; i < 128; i++) {
-            if(freqSB[i].length() == 0) {
+            if(freqSB[i] == null) {
                 break;
             }
             res.append(freqSB[i]);
