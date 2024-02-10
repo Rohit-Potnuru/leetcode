8cbff6432ff3c9be9061eb0cf113c0715a16ad9c
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-09 19:58:18 -0600

    Time: 3 ms (67.98%), Space: 41.4 MB (71.73%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,22 +1,22 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length(), j;
-        int count = 0;
+        int n = s.length(), upper;
+        int count = n;
         for(int i = 0; i < n; i++) {
-            j = 0;
-            while(i - j >= 0 
-                  && i + j < n 
-                  && s.charAt(i - j) == s.charAt(i + j)) {
+            upper = Math.min(i + 1, n - i);
+            for(int j = 1; j < upper; j++) {
+                if(s.charAt(i - j) != s.charAt(i + j)) {
+                    break;
+                }
                 count++;
-                j++;
             }
-            
-            j = 0;
-            while(i - j >= 0 
-                  && i + j + 1 < n 
-                  && s.charAt(i - j) == s.charAt(i + j + 1)) {
+
+            upper = Math.min(i + 1, n - i - 1);
+            for(int j = 0; j < upper; j++) {
+                if(s.charAt(i - j) != s.charAt(i + j + 1)) {
+                    break;
+                }
                 count++;
-                j++;
             }
         }
         return count;
