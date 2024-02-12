d6d0cee0363bf4eb40086370c45f9d658f0f75f8
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-11 19:53:29 -0600

    Time: 16 ms (99.64%), Space: 45.5 MB (76.13%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
@@ -1,28 +1,42 @@
 class Solution {
-    boolean isSubsequence(String s, String t) {
-        int n = t.length(), start = 0;
-        for(char ch: s.toCharArray()) {
-            if(ch == t.charAt(start)) {
-                start++;
-            }
-            if(start == n) {
-                return true;
-            }
+public int numMatchingSubseq(String s, String[] words) {
+        Word[] heads = new Word[26];
+        // fill the heads
+        for (int i = 0; i < 26; i++) heads[i] = new Word("", 0);
+        for (String word : words) {
+            Word head = heads[word.charAt(0) - 'a'];
+            Word newWord = new Word(word, 0);
+            newWord.next = head.next;
+            head.next = newWord;
         }
-        return false;
-    }
-    public int numMatchingSubseq(String s, String[] words) {
         int count = 0;
-        HashMap<String, Integer> hashmap = new HashMap<>();
-        for(String word: words) {
-            hashmap.put(word, hashmap.getOrDefault(word, 0) + 1);
-        }
-        
-        for(String word: hashmap.keySet()) {
-            if(isSubsequence(s, word)) {
-                count += hashmap.get(word);
+        for (char c : s.toCharArray()) {
+            Word curHead = heads[c - 'a'];
+            Word cur = curHead.next;
+            curHead.next = null;
+            while (cur != null) {
+                Word next = cur.next;
+                if (cur.index == cur.word.length() - 1) {
+                    count++;
+                } else {
+                    cur.index++;
+                    Word nextHead = heads[cur.word.charAt(cur.index) - 'a'];
+                    cur.next = nextHead.next;
+                    nextHead.next = cur;
+                }
+                cur = next;
             }
         }
         return count;
+    }
+    
+    class Word {
+        String word;
+        int index;
+        Word next;
+        Word(String word, int index) {
+            this.word = word;
+            this.index = index;
+        }
     }
 }
