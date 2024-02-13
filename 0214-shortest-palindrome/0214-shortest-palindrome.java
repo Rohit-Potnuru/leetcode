c56a67cb014fb54f3ecf5611163392a9fe43361f
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-12 19:52:00 -0600

    Time: 6 ms (88.16%), Space: 45 MB (25%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,43 +1,11 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        int n = s.length();
-        int[] lsp = new int[n];
-        lsprefix(s.toCharArray(), lsp);
-        int start = 0;
-        int end = n - 1;
-
-        while(end >= 0) {
-            if(s.charAt(start) == s.charAt(end)) {
-                end--;
-                start++;
-            }
-            else {
-                if(start != 0) {
-                    start = lsp[start - 1];
-                }
-                else {
-                    end--;
-                }
-            }
+        int j = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == s.charAt(j)) { j += 1; }
         }
-
-        return new StringBuilder(s.substring(start)).reverse().toString() + s;
-    }
-
-    public void lsprefix(char[] p, int[] lsp) {
-        int n = p.length, start = 0, end = 1;
-        while(end < n) {
-            if(p[start] == p[end]) {
-                lsp[end++] = ++start;
-            }
-            else {
-                if(start != 0) {
-                    start = lsp[start - 1];
-                }
-                else {
-                    lsp[end++] = start;
-                }
-            }
-        }
+        if (j == s.length()) { return s; }
+        String suffix = s.substring(j);
+        return new StringBuffer(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
     }
 }
