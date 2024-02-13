81318e9b9da3ff91b1e9cfd45d7766484dc04910
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-12 18:43:32 -0600

    Time: 18 ms (99.64%), Space: 45.8 MB (57.96%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
@@ -2,25 +2,27 @@ class Solution {
     public int numMatchingSubseq(String s, String[] words) {
         Word[] headWords = new Word[26];
         Word curr, temp;
+        for(int i = 0; i < 26; i++) headWords[i] = new Word("", 0, null);
         for(String word : words) {
-            temp = headWords[word.charAt(0) - 'a'];
-            headWords[word.charAt(0) - 'a'] = new Word(word, 0, temp);
+            temp = headWords[word.charAt(0) - 'a'].next;
+            headWords[word.charAt(0) - 'a'].next = new Word(word, 0, temp);
         }
 
         int count = 0;
         for(char ch: s.toCharArray()) {
-            curr = headWords[ch - 'a'];
+            curr = headWords[ch - 'a'].next;
+            headWords[ch - 'a'].next = null;
             while(curr != null) {
-                headWords[ch - 'a'] = curr.next;
                 temp = curr.next;
 
                 curr.index++;
                 if(curr.index == curr.word.length()) {
                     count++;
+                    curr.next = null;
                 }
                 else {
-                    curr.next = headWords[curr.word.charAt(curr.index) - 'a'];
-                    headWords[curr.word.charAt(curr.index) - 'a'] = curr;
+                    curr.next = headWords[curr.word.charAt(curr.index) - 'a'].next;
+                    headWords[curr.word.charAt(curr.index) - 'a'].next = curr;
                 }
                 curr = temp;
             }
