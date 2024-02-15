600cebeb052632155040537049858d124716239b
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-14 20:12:37 -0600

    Time: 1 ms (90.38%), Space: 41.4 MB (75.31%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int maxScoreWords(int[][] freq_words, int pos, int[] score, int[] freq_letters) {
+    public int maxScoreWords(int pos, int[][] freq_words, int[] word_scores, int[] freq_letters) {
         if(pos >= freq_words.length) return 0;
-        int maxScore = maxScoreWords(freq_words, pos + 1, score, freq_letters);
+        int maxScore = maxScoreWords(pos + 1, freq_words, word_scores, freq_letters);
 
         for(int i = 0; i < 26; i++) {
             if(freq_words[pos][i] > freq_letters[i]) {
                 return maxScore;
             }
         }
-        int word_score = 0;
+
         for(int i = 0; i < 26; i++) {
-            word_score += score[i] * freq_words[pos][i];
             freq_letters[i] -= freq_words[pos][i];
         }
 
-        maxScore = Math.max(maxScore, word_score + maxScoreWords(freq_words, pos + 1, score, freq_letters));
+        maxScore = Math.max(maxScore, word_scores[pos] + maxScoreWords(pos + 1, freq_words, word_scores, freq_letters));
 
         for(int i = 0; i < 26; i++) {
             freq_letters[i] += freq_words[pos][i];
@@ -26,14 +25,16 @@ class Solution {
         int n = words.length;
         int[] freq_letters = new int[26];
         int[][] freq_words = new int[n][26];
+        int[] word_scores = new int[n];
         for(char ch: letters) {
             freq_letters[ch - 'a']++;
         }
         for(int i = 0; i < n; i++) {
             for(char ch: words[i].toCharArray()) {
                 freq_words[i][ch - 'a']++;
+                word_scores[i] += score[ch - 'a'];
             }
         }
-        return maxScoreWords(freq_words, 0, score, freq_letters);
+        return maxScoreWords(0, freq_words, word_scores, freq_letters);
     }
 }
