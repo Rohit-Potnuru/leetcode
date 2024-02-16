d84b802812e388ad39b302e25135cc8445301d21
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-02-15 19:19:41 -0600

    Time: 32 ms (97.51%), Space: 56.8 MB (80%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,27 +1,20 @@
 class Solution {
-    public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        Arrays.sort(arr);
-        int prev = arr[0], count = 0;
-        ArrayList<Integer> freqCount = new ArrayList<>();
-        for(int a: arr) {
-            if(prev != a) {
-                freqCount.add(count);
-                prev = a;
-                count = 0;
+        public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int a : arr)
+            count.put(a, 1 + count.getOrDefault(a, 0));
+        int remaining = count.size(), occur = 1;
+        int[] occurrenceCount = new int[arr.length + 1];
+        for (int v : count.values())
+            ++occurrenceCount[v];
+        while (k > 0) {
+            if (k - occur * occurrenceCount[occur] >= 0) {
+                k -= occur * occurrenceCount[occur];
+                remaining -= occurrenceCount[occur++];
+            }else {
+                return remaining - k / occur;
             }
-            count++;
-            prev = a;
         }
-        freqCount.add(count);
-        Collections.sort(freqCount);
-
-        count = freqCount.size();
-        int j = 0;
-        while(j < freqCount.size() && k > 0) {
-            k -= freqCount.get(j++);
-        }
-        if(k != 0) j--;
-
-        return count - j;
+        return remaining;        
     }
 }
