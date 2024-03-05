833e9f1de18cfe8592bd4e7dff9e4955da196aca
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-03-04 20:40:15 -0600

    Time: 3 ms (100%), Space: 45.6 MB (10.16%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -6,15 +6,13 @@ class Solution {
             char strChar = s.charAt(start);
             char endChar = s.charAt(end);
             if(strChar != endChar || start >= end) break;
-            while(start + 1 < n  && strChar == s.charAt(start + 1)) {
+            while(start < n  && strChar == s.charAt(start)) {
                 start += 1;
             }
 
-            while(end - 1 >= 0  && endChar == s.charAt(end - 1)) {
+            while(end >= 0  && endChar == s.charAt(end)) {
                 end -= 1;
             }
-            start += 1;
-            end -= 1;
         }
         return end - start + 1 > 0? end - start + 1: 0;
     }
