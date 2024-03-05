f374f3a9a234bd73d2f830734cae706ebb121b01
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-03-05 17:18:06 -0600

    Time: 2 ms (89.26%), Space: 45.1 MB (40.17%) - LeetHub

---
+++ 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumOperationsToWriteY(int[][] grid) {
+        int n = grid.length;
+        int[] yCellCount = new int[3];
+        int[] nonYCellCount = new int[3];
+        int yCells = 3 * n/2 + 1, nonYCells = n*n;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if((i >= n/2 && j == n/2) || 
+                   (i < n/2 && (i == n - j - 1 || i == j))) {
+                    yCellCount[grid[i][j]]++;
+                }
+                else {
+                    nonYCellCount[grid[i][j]]++;
+                }
+            }
+        }
+        nonYCells -= yCells;
+        
+        int minCount = Integer.MAX_VALUE, total;
+        for(int i = 0; i < 3; i++) {
+            total = yCells - yCellCount[i] + nonYCells - Math.max(nonYCellCount[(i + 1)%3], nonYCellCount[(i + 2)%3]);
+            minCount = Math.min(minCount, total);
+        }
+        return minCount;
+    }
+} 
