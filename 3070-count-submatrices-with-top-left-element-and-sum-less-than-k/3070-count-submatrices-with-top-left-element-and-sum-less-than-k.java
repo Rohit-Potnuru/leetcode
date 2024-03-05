5b358302a821b3a496891ca1b5ab76e6f58842a7
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-03-05 17:12:22 -0600

    Time: 3 ms (97.96%), Space: 117.5 MB (83.64%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -2,20 +2,19 @@ class Solution {
     public int countSubmatrices(int[][] grid, int k) {
         int m = grid.length, n = grid[0].length;
         int count = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                grid[i][j] += grid[i][j - 1];
-            }
+        if(grid[0][0] <= k) count++;
+        for(int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+            if(grid[i][0] <= k) count++;
+        }
+        for(int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+            if(grid[0][j] <= k) count++;
         }
         for(int i = 1; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                grid[i][j] += grid[i - 1][j];
-            }
-        }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+            for(int j = 1; j < n; j++) {
+                grid[i][j] += grid[i - 1][j] + grid[i][j - 1] - grid[i - 1][j - 1];
                 if(grid[i][j] <= k) count++;
-                else break;
             }
         }
         return count;
