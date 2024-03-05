84d5f9a64c261f14c1251f972dbcf384d6a30c70
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-03-05 17:13:04 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -7,15 +7,18 @@ class Solution {
         for(int i = 1; i < m; i++) {
             grid[i][0] += grid[i - 1][0];
             if(grid[i][0] <= k) count++;
+            else break;
         }
         for(int j = 1; j < n; j++) {
             grid[0][j] += grid[0][j - 1];
             if(grid[0][j] <= k) count++;
+            else break;
         }
         for(int i = 1; i < m; i++) {
             for(int j = 1; j < n; j++) {
                 grid[i][j] += grid[i - 1][j] + grid[i][j - 1] - grid[i - 1][j - 1];
                 if(grid[i][j] <= k) count++;
+                else break;
             }
         }
         return count;
