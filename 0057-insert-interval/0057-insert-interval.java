07ce20ef6d87c1b09ee8e77490705958b761a6a0
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-03-17 15:51:01 -0500

    Time: 3 ms (19.04%), Space: 44.6 MB (85.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,28 +1,27 @@
 class Solution {
-    class The_Comparator implements Comparator<int[]> {
-        public int compare(int[] i1, int[] i2) {
-            return i1[0] - i2[0];
-        }
-    }
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new The_Comparator());
-        pq.add(newInterval);
-        for(int[] interval: intervals) {
-            pq.add(interval);
-        }
-
         ArrayList<int[]> ans = new ArrayList<>();
-        int n = -1;
-        while(!pq.isEmpty()) {
-            int[] temp = pq.poll();
-            if(n >= 0 && Math.max(temp[0], ans.get(n)[0]) <= Math.min(temp[1], ans.get(n)[1])) {
-                ans.get(n)[1] = Math.max(temp[1], ans.get(n)[1]);
-            } 
-            else {
-                ans.add(temp);
-                n++;
-            }
+        int n = intervals.length;
+        int i = 0, size = -1;
+        while(i < n) {
+            if(newInterval[0] <= intervals[i][1]) break;
+            ans.add(intervals[i]);
+            i++;
+            size++;
         }
-        return ans.toArray(new int[n + 1][]);
+        ans.add(newInterval);
+        size++;
+        while(i < n) {
+            if(Math.max(intervals[i][0], ans.get(size)[0]) <= Math.min(intervals[i][1], ans.get(size)[1])) {
+                ans.get(size)[0] = Math.min(intervals[i][0], ans.get(size)[0]);
+                ans.get(size)[1] = Math.max(intervals[i][1], ans.get(size)[1]);
+            }
+            else {
+                ans.add(intervals[i]);
+                size++;
+            }
+            i++;
+        }
+        return ans.toArray(new int[size + 1][]);
     }
 }
