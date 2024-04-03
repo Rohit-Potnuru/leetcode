8d3e971799612b7149a6ddb7c64c8f9ab4fab019
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-04-03 16:23:56 -0500

    Time: 265 ms (14.99%), Space: 45 MB (13.1%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean dfs(char[][] board, String word, int wordPos, int posx, int posy) {
+        if(wordPos == word.length() - 1) return board[posx][posy] == word.charAt(wordPos);
+        if(board[posx][posy] != word.charAt(wordPos)) return false;
+        int m = board.length, n = board[0].length;
+        int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+        char temp = board[posx][posy];
+        board[posx][posy] = '.';
+        boolean flag = false;
+        for(int[] neighbor : directions) {
+            int dx = posx + neighbor[0];
+            int dy = posy + neighbor[1];
+
+            if(0 <= dx && dx < m && 0 <= dy && dy < n) {
+                flag |= dfs(board, word, wordPos + 1, dx, dy);
+            }
+        }
+        board[posx][posy] = temp;
+        return flag;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dfs(board, word, 0, i, j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
