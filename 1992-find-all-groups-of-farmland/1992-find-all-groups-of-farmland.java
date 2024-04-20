1ca2ea2de81659a6e042499c42fa97efb8faf74e
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-04-19 21:54:25 -0500

    Time: 518 ms (5.32%), Space: 59.8 MB (80.99%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] findFarmland(int[][] land) {
+        int rows = land.length, cols = land[0].length;
+        ArrayList<int[]> ans = new ArrayList<>();
+        int count = 1, temp;
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(land[i][j] == 1) {
+                    System.out.println(i + " " + j + " " + (i > 0 && land[i - 1][j] < 0) +  (j > 0 && land[i][j - 1] < 0));
+                    if((i > 0 && land[i - 1][j] < 0) || (j > 0 && land[i][j - 1] < 0)) {
+                        temp = (i > 0 && land[i - 1][j] < 0) ? -land[i - 1][j]: -land[i][j - 1];
+                        ans.get(temp - 1)[2] = i;
+                        ans.get(temp - 1)[3] = j;
+                        land[i][j] = -temp;
+                    }
+                    else {
+                        land[i][j] = -count;
+                        count++;
+                        ans.add(new int[]{i, j, i, j});
+                    }
+                }
+            }
+        }
+        return ans.toArray(new int[ans.size()][4]);
+    }
+}
