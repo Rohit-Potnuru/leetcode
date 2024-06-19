4e86179946848b3618efdfe432bcd3cb593d4de4
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-06-19 09:55:35 -0500

    Time: 21 ms (53.6%), Space: 57.8 MB (57.51%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -2,8 +2,8 @@ class Solution {
     private int calculate_bouquet(int[] bloomDay, int k, int val) {
         int count = 0;
         int n_bouquets = 0;
-        for(int bloom: bloomDay) {
-            if(bloom <= val) {
+        for(int end: bloomDay) {
+            if(end <= val) {
                 count++;
             }
             else {
@@ -20,7 +20,11 @@ class Solution {
         if(m * k > n) {
             return -1;
         }
-        int start = 0, end = 1000_000_001, n_bouquets;
+        int start = 0, end = 1000_000_001, maxEnd = 0, n_bouquets;
+        for (int day : bloomDay) {
+            maxEnd = Math.max(maxEnd, day);
+        }
+        end = ++maxEnd;
         while(start < end) {
             int mid = start + (end - start)/2;
             n_bouquets = calculate_bouquet(bloomDay, k, mid);
@@ -31,6 +35,6 @@ class Solution {
                 start = mid + 1;
             }
         }
-        return start == 1000_000_001 ? -1 : start;
+        return start == maxEnd ? -1 : start;
     }
 }
