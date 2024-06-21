19aae477cd53407be2f9c508d0a4f5f5154370b8
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-06-21 17:53:40 -0500

    Time: 22 ms (63.49%), Space: 43.9 MB (82.54%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,35 +1,25 @@
 class Solution {
-    private Float[] getSlope_n_intercept(int[] p1, int[] p2) {
-        Float slope = (float)Integer.MAX_VALUE, intercept;
+    private Float getSlope_n_intercept(int[] p1, int[] p2) {
+        Float slope = (float)Integer.MAX_VALUE;
         if(p1[0] != p2[0]) {
             slope = (float)(p1[1] - p2[1])/(float)(p1[0] - p2[0]);
             slope = (Math.abs(slope) == 0) ? 0 : slope;
         }
-
-        intercept = (float)(p1[1]) - slope * ((float)p1[0]);
-        return new Float[]{slope, intercept};
+        return slope;
     }
 
     public int maxPoints(int[][] points) {
-        HashMap<Float, HashMap<Float, int[]>> lineMap = new HashMap<>();
+        HashMap<Float, Integer> lineMap = new HashMap<>();
         int n = points.length, maxPoints = 0;
         for(int i = 0; i < n; i++) {
+            lineMap = new HashMap<>();
             for(int j = i + 1; j < n; j++) {
-                Float[] si = getSlope_n_intercept(points[i], points[j]);
-                Float slope = si[0], intercept = si[1];
+                Float slope = getSlope_n_intercept(points[i], points[j]);
                 if(!lineMap.containsKey(slope)) {
-                    lineMap.put(slope, new HashMap<>());
+                    lineMap.put(slope, 0);
                 }
-                if(!lineMap.get(slope).containsKey(intercept)) {
-                    lineMap.get(slope).put(intercept, new int[]{i, 0});
-                }
-                System.out.println(slope + " " + intercept);
-                if(lineMap.get(slope).get(intercept)[0] == i) {
-                    int[] temp = lineMap.get(slope).get(intercept);
-                    temp[1]++;
-                    lineMap.get(slope).put(intercept, temp);
-                    maxPoints = Math.max(maxPoints, temp[1]);
-                }
+                lineMap.put(slope, lineMap.get(slope) + 1);
+                maxPoints = Math.max(maxPoints, lineMap.get(slope));
             }
         }
         return maxPoints + 1;
