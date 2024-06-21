28ceefb06d17a39464895c72b5b69407f838ca97
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2024-06-21 14:12:49 -0500

    Time: 391 ms (5.07%), Space: 63.1 MB (37.44%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -10,35 +10,25 @@ class Solution {
             }
         });
         
-        PriorityQueue<int[]> profitPQ = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] cp1, int[] cp2) {
-                if(cp1[1] == cp2[1]) {
-                    return Integer.compare(cp1[0], cp2[0]);
-                }
-                return Integer.compare(cp2[1], cp1[1]);
-            }
-        });
+        PriorityQueue<Integer> profitPQ = new PriorityQueue<>();
 
         int n = capital.length;
         for(int i = 0; i < n; i++) {
             capitalPQ.add(new int[]{capital[i], profits[i]});
         }
 
-        int ans = w;
         while(k > 0) {
             while(!capitalPQ.isEmpty() && capitalPQ.peek()[0] <= w) {
-                profitPQ.add(capitalPQ.poll());
+                profitPQ.add(-capitalPQ.poll()[1]);
             }
             if(profitPQ.isEmpty()) {
                 break;
             }
-            
-            int[] tempProject = profitPQ.poll();
-            w += tempProject[1];
-            ans += tempProject[1];
+            int temp = profitPQ.poll();
+            System.out.println(temp);
+            w -= temp;
             k--;
         }
-        return ans;
+        return w;
     }
 }
