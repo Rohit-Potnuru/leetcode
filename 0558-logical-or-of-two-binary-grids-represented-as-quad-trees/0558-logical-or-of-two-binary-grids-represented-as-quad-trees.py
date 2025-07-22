c7a39b6d188dc9e3910cfdaf86099e87c2ea3fc8
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2025-07-21 20:43:20 -0500

    Time: 47 ms (74.29%), Space: 13.5 MB (22.86%) - LeetHub

---
+++ 0558-logical-or-of-two-binary-grids-represented-as-quad-trees/0558-logical-or-of-two-binary-grids-represented-as-quad-trees.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a QuadTree node.
+class Node(object):
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution(object):
+    def intersect(self, quadTree1, quadTree2):
+        """
+        :type quadTree1: Node
+        :type quadTree2: Node
+        :rtype: Node
+        """
+        if quadTree1.isLeaf:
+            return quadTree1 if quadTree1.val else quadTree2
+
+        elif quadTree2.isLeaf:
+            return quadTree2 if quadTree2.val else quadTree1
+        else:
+            topLeft = self.intersect(quadTree1.topLeft, quadTree2.topLeft)
+            topRight = self.intersect(quadTree1.topRight, quadTree2.topRight)
+            bottomLeft = self.intersect(quadTree1.bottomLeft, quadTree2.bottomLeft)
+            bottomRight = self.intersect(quadTree1.bottomRight, quadTree2.bottomRight)
+            leafVal = topLeft.val == topRight.val == bottomLeft.val == bottomRight.val
+            isLeafVal = topLeft.isLeaf == topRight.isLeaf == bottomLeft.isLeaf == bottomRight.isLeaf == True
+            if leafVal and isLeafVal:
+                return Node(topLeft.val, True)
+            return Node(quadTree1.val or quadTree2.val, False, topLeft, topRight, bottomLeft, bottomRight) 
