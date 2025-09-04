ab5855664c9d824d0b701f4ab4346b4af2de4066
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2025-09-03 20:02:14 -0500

    Time: 15 ms (6.41%), Space: 12.7 MB (26.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -6,34 +6,42 @@ class Solution(object):
         ["flower","flow","flight"] -> "fl"
         ["flower","flow","flight", "flig] -> 
         """
-        self.prefix = strs[0]
-        b = self.Trie()
-        b.insert(self.prefix)
+        trie = self.Trie()
 
-        for word in strs[1:]:
-            current = b
-            for ch in range(0, len(word)):
-                val = ord(word[ch]) - ord("a")
-                if current.children[val] is None :
-                    self.prefix = self.prefix[:ch]
-                    break
-                current = current.children[val]
-            if len(self.prefix) >  len(word):
-                self.prefix = word
-        return self.prefix
-    
+        for word in strs:
+            trie.insert(word)
+        prefix_len = trie.search(strs[0], len(strs))
+        print(prefix_len)
+        if prefix_len == 0:
+            return ""
+        return strs[0][:prefix_len]
+        
     class Trie(object):
         def __init__(self):
-            self.isEnd = False
+            self.count = 0
             self.children = [None] * 26
         
         def insert(self, word):
             current = self
-
+            current.count += 1
             for ch in word:
                 norm = ord(ch) - ord("a")
                 if current.children[norm] is None:
                     current.children[norm] = Solution.Trie()
                 current = current.children[norm]
-            current.isEnd = True
+                current.count += 1
+
+
+        def search(self, word, length):
+            current = self
+            count = 0
+            for ch in word:
+                norm = ord(ch) - ord("a")
+                if current.children[norm] is None or current.children[norm].count != length:
+                    return count
+                current = current.children[norm]
+                count += 1
+            return count
+
+
             
