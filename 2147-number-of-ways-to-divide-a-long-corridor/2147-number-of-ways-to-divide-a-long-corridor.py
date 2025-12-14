df6e8b8d52280f997e7f9ac581cb202a963f07da
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2025-12-13 22:42:40 -0600

    Time: 7318 ms (18.18%), Space: 12.8 MB (100%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    # def numberOfWays(self, s):
+    #     a = [i for i,c in enumerate(s) if c == 'S']
+    #     res = 1
+    #     for i in xrange(1,len(a) - 1,2):
+    #         res *= a[i+1] - a[i]
+    #         print(a[i + 1], a[i+1] - a[i])
+    #     return res % (10**9+7) * (len(a) % 2 == 0 and len(a) >= 2) 
+
+    def numberOfWays(self, corridor):
+        """
+        :type corridor: str
+        :rtype: int
+        """
+        MODULO = 10**9 + 7
+        totalCount = 1
+        chairCount = 0
+        totChairCount = 0
+        i, n = 0, len(corridor)
+        while i < n:
+            if corridor[i] == 'S':
+                chairCount += 1
+                totChairCount += 1
+            i += 1
+
+            if chairCount == 2:
+                chairCount = 0
+                previ = i
+                while i < n and corridor[i] == 'P':
+                    i += 1
+                if i == n:
+                    break
+                totalCount *= i - previ + 1
+                print(i, i - previ + 1)
+        if totChairCount != 0 and totChairCount % 2 == 0:
+            return int(totalCount % MODULO)
+        return 0
