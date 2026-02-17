2b7d9b315cb3451c8ee93536b5fa5436ead7ca8e
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-02-17 15:51:26 -0600

    Time: 3 ms (77.76%), Space: 20.4 MB (82%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -6,21 +6,51 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-         inorder_idx = {val: idx for idx, val in enumerate(inorder)}
+        idxMap = dict()
+        for idx, val in enumerate(inorder):
+            idxMap[val] = idx
         
-        root = TreeNode(postorder[-1])
-        stack = [root]
+        root = TreeNode(-1)
+        stack = [(root, -1)]
+        for val in reversed(postorder):
+            node = TreeNode(val)
+            idx = idxMap[val]
 
-        for node in reversed(postorder[:-1]):
-            curr_idx = inorder_idx[node]
-            curr_node = TreeNode(node)
-            
-            if inorder_idx[stack[-1].val] < curr_idx:
-                stack[-1].right = curr_node
+            if stack[-1][1] < idx:
+                stack[-1][0].right = node
             else:
-                parent = None
-                while stack and inorder_idx[stack[-1].val] > curr_idx:
-                    parent = stack.pop()
-                parent.left = curr_node
-            stack.append(curr_node)
-        return root
+                prev = None
+                while stack[-1][1] > idx:
+                    prev = stack.pop()[0]
+                prev.left = node
+            stack.append((node, idx))
+        return root.right
+        '''
+        
+              1
+            2   3
+           4 5 6 7
+
+        4 2 5 1 6 3 7
+        4 5 2 6 7 3 1
+        1 3 7 6 2 5 4
+
+        1 6
+
+        L ro R
+        L R ro
+
+        1 2 4 5 3 6 7
+        '''
+        idxMap = dict()
+        for idx, val in enumerate(postorder):
+            idxMap[val] = idx
+
+        root = TreeNode(-1)
+        stack = [(root, len(inorder))]
+
+        for val in inorder:
+            node = TreeNode(val)
+            idx = idxMap[val]
+
+            stack.append(node)
