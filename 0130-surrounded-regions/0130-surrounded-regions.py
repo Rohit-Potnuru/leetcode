7ccbfd453b7bb37add8f47788b1c43335158e43c
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-02-26 11:08:08 -0600

    Time: 7 ms (55.92%), Space: 22.3 MB (76.39%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,19 +1,10 @@
 class Solution:
-    def getNeighbors(self, board: List[List[str]], i: int, j: int, m: int, n: int) -> List[List[str]]:
-        neighbors = [[1, 0], [0, -1], [-1, 0], [0, 1]]
-        res = []
-        for di, dj in neighbors:
-           new_i = i + di
-           new_j = j + dj
-           if 0 <= new_i < m and 0 <= new_j < n and board[new_i][new_j] == "O":
-               res.append([new_i, new_j])
-        return res
-
     def dfs(self, board: List[List[str]], i: int, j: int, m: int, n: int) -> None:
+        if not (0 <= i < m and 0 <= j < n) or board[i][j] != "O":
+            return
         board[i][j] = "D"
-        neighbors = self.getNeighbors(board, i, j, m, n)
-        for neig in neighbors:
-            self.dfs(board, neig[0], neig[1], m, n)
+        for di, dj in [[1, 0], [0, -1], [-1, 0], [0, 1]]:
+            self.dfs(board, i + di, j + dj, m, n)
         
     def solve(self, board: List[List[str]]) -> None:
         """
@@ -22,16 +13,12 @@ class Solution:
         m = len(board)
         n = len(board[0])
         for i in range(m):
-            if board[i][0] == "O":
-                self.dfs(board, i, 0, m, n)
-            if board[i][n - 1] == "O":
-                self.dfs(board, i, n - 1, m, n)
+            self.dfs(board, i, 0, m, n)
+            self.dfs(board, i, n - 1, m, n)
         
         for j in range(n):
-            if board[0][j] == "O":
-                self.dfs(board, 0, j, m, n)
-            if board[m - 1][j] == "O":
-                self.dfs(board, m - 1, j, m, n)
+            self.dfs(board, 0, j, m, n)
+            self.dfs(board, m - 1, j, m, n)
 
         for i in range(m):
             for j in range(n):
