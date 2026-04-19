84c20c126138724cb7e2631a78e4deeb339582cc
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-19 12:43:21 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,11 +1,15 @@
 class Solution:
+    def backtrack(self, start, comb):
+        if len(comb) == self.k:
+            self.res.append(comb[:])
+            return
+        for i in range(start, self.n + 1):
+            comb.append(i)
+            self.backtrack(i + 1, comb)
+            comb.pop()
     def combine(self, n: int, k: int) -> List[List[int]]:
-        prevRes = [[i + 1] for i in range(n)]
-        res = prevRes
-        for pos in range(2, k + 1):
-            res = []
-            for r in prevRes:
-                for i in range(r[-1] + 1, n + 1):
-                    res.append(r + [i])
-            prevRes = res
-        return res
+        self.res = []
+        self.k = k
+        self.n = n
+        self.backtrack(1, [])
+        return self.res
