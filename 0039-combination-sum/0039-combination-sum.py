954f83f8f8184ca6bda4175cb50f160eb34dcbf5
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-19 13:57:28 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,14 +3,14 @@ class Solution:
         if target == 0:
             self.res.append(comb[:])
             return
-        elif target < 0:
-            return
         
         for i in range(pos, len(self.candidates)):
-            if target >= self.candidates[i]:
-                comb.append(self.candidates[i])
-                self.genCombinationSum(comb, target - self.candidates[i], i)
-                comb.pop()
+            if target < self.candidates[i]:
+                break
+                
+            comb.append(self.candidates[i])
+            self.genCombinationSum(comb, target - self.candidates[i], i)
+            comb.pop()
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         sortedCandidates = sorted(candidates)
