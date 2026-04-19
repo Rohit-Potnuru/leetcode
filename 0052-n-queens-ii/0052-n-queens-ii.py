7a19b8d24e176ff51e308afc23dbecaa30d65fe9
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-19 17:20:03 -0500

    Time: 1159 ms (5.01%), Space: 19.5 MB (13.29%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        horizontal = set()
+        vertical = set()
+        diagLeftRight = set()
+        diagRightLeft = set()
+        self.res = 0
+        def backTrack(x: int, y: int, t: int):
+            if t == 0:
+                self.res += 1
+                return
+            i, j = x, y
+            while i < n:
+                while j < n:
+                    if ((i not in horizontal) and
+                       (j not in vertical) and
+                       (i - j not in diagLeftRight) and
+                       (i + j - n not in diagRightLeft)):
+                        horizontal.add(i)
+                        vertical.add(j)
+                        diagLeftRight.add(i - j)
+                        diagRightLeft.add(i + j - n)
+                        backTrack(i, j + 1, t - 1)
+                        horizontal.remove(i)
+                        vertical.remove(j)
+                        diagLeftRight.remove(i - j)
+                        diagRightLeft.remove(i + j - n)
+                    j += 1
+                j = 0
+                i += 1
+        backTrack(0, 0, n)
+        return self.res
+
+
