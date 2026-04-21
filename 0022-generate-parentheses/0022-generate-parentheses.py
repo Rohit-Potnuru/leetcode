3be168b1653e2748f85e42fa36d9524582effe49
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-20 22:11:16 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,7 +1,11 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        if not hasattr(self, 'memo'):
+            self.memo = {}
         if n == 1:
             return ["()"]
+        elif n in self.memo:
+            return self.memo[n]
         else:
             res = set()
             for i in range(1, n):
@@ -12,4 +16,5 @@ class Solution:
                         res.add(leftPranthesis + rightPranthesis)
                     if i == 1:
                         res.add('(' + rightPranthesis + ')')
-            return list(res)
+            self.memo[n] = list(res)
+            return self.memo[n]
