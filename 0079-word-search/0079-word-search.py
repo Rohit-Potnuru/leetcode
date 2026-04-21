cf275943d8e6c8e2d1343a1f84b2b7bb74c953ee
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-20 22:42:33 -0500

    Time: 4593 ms (24.55%), Space: 19.4 MB (61.13%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def backTrack(x: int, y: int, pos: int):
+            if board[x][y] != word[pos]:
+                return False
+            if pos == len(word) - 1:
+                return board[x][y] == word[pos]
+
+            temp = board[x][y]
+            board[x][y] = "."
+            ans = False
+            for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+                new_x = x + dx
+                new_y = y + dy
+                if 0 <= new_x < n and 0 <= new_y < m:
+                    ans = backTrack(new_x, new_y, pos + 1) or ans
+                if ans:
+                    break
+            board[x][y] = temp
+            return ans
+        n = len(board)
+        m = len(board[0])
+        for i in range(n):
+            for j in range(m):
+                ans = backTrack(i, j, 0)
+                if ans:
+                    return True
+        return False
