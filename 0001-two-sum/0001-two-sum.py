38b65c4e7ed63b93af8e23b295d2f0d3f915200b
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-26 22:26:27 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,18 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = dict()
         for i, num in enumerate(nums):
-            if num not in hashmap:
-                hashmap[num] = []
-            hashmap[num].append(i)
-
-        for i, num in enumerate(nums):
             val = target - num
             if val in hashmap:
-                positions = hashmap[val]
-                for pos in positions:
-                    if pos != i:
-                        return [pos, i]
-        
+                return [i, hashmap[val]]
+            hashmap[num] = i
 
 # [2,7,11,15] target = 9
 
