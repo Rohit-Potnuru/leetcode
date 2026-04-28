e1de756f68568c026d6b526a24bdce9db3e6680f
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-28 15:19:59 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,10 +4,10 @@ class Solution:
         voc = defaultdict(int)
         i = 0
         maxx = 0
-        for j in range(len(s)):
-            voc[s[j]] = voc[s[j]] + 1
-            while voc[s[j]] > 1:
-                voc[s[i]] = voc[s[i]] - 1
+        for j, ch in enumerate(s):
+            voc[ch] += 1
+            while voc[ch] > 1:
+                voc[s[i]] -= 1
                 i += 1
             maxx = max(maxx, j - i + 1)
         return maxx
