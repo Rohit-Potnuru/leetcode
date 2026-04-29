65734e03d827557c508f2bd97a513bfaa3d363fc
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-28 21:39:36 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,13 +1,17 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def validPalindrome(i: int, j: int, delCount: int):
-            if i >= j:
-                return True
-            elif s[i] == s[j]:
-                return validPalindrome(i + 1, j - 1, delCount)
-            elif delCount > 0:
-                return validPalindrome(i, j - 1, delCount - 1) or validPalindrome(i + 1, j, delCount - 1)
-            else:
-                return False
-            
-        return validPalindrome(0, len(s) - 1, 1)
+        def isPalin(i: int, j: int) -> bool:
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return isPalin(i + 1, j) or isPalin(i, j - 1)
+            i += 1
+            j -= 1
+        return True
