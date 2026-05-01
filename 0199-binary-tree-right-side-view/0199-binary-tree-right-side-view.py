153fb084098bba3fe9a90402999e2c1c51598595
Rohit Potnuru <69067810+Rohit-Potnuru@users.noreply.github.com>
2026-04-30 21:12:30 -0500

    Time: 0 ms (100%), Space: 19.3 MB (32.67%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -5,16 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def preOrder(self, root, depth):
-        if root is None:
-            return
-        if len(self.view) == depth:
-            self.view.append(0)
-        self.view[depth] = root.val
-        self.preOrder(root.left, depth + 1)
-        self.preOrder(root.right, depth + 1)
-
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        self.view = []
-        self.preOrder(root, 0)
-        return self.view
+        self.rightView = []
+        def dfs(root, depth):
+            if root is None:
+                return
+            if len(self.rightView) == depth:
+                self.rightView.append(0)
+            self.rightView[depth] = root.val
+            dfs(root.left, depth + 1)
+            dfs(root.right, depth + 1)
+        dfs(root, 0)
+        return self.rightView 
